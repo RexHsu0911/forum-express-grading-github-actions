d9ba3210ed57a64b3f9ca9acc69e4189d0bb7ace
Rex Hsu <aexz5476@gmail.com>
2023-10-01 12:10:16 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 3000
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
+
+// 當我們用 POST 動作來傳送資料時，來解析 request 的內容(Express 已內建 body-parser，直接引入使用)
+app.use(express.urlencoded({ extended: true }))
+
 app.use(routes)
 
 app.listen(port, () => {
