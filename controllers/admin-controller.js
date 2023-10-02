8eea431a045da7d4bff493c5dad8576b4a22b310
Rex Hsu <aexz5476@gmail.com>
2023-10-02 14:26:42 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,19 @@
+// 等同於 const db = require('../models')
+// const Restaurant = db.Restaurant
+// 採用解構賦值的寫法
+const { Restaurant } = require('../models')
+
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      // {raw: true} 把 Sequelize 包裝過的一大包物件轉換成格式比較單純的 JS 原生物件
+      raw: true
+    })
+      .then(restaurants => {
+        res.render('admin/restaurants', { restaurants })
+        console.log('restaurants', restaurants)
+      })
+      .catch(err => next(err))
   }
 }
 
