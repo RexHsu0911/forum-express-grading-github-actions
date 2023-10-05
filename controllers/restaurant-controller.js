cdfe129bf8b8f4c7731084b80b8efcbce9f3619b
Rex Hsu <aexz5476@gmail.com>
2023-10-06 01:13:50 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -6,17 +6,30 @@ const restaurantController = {
       include: Category,
       nest: true,
       raw: true
-    }).then(restaurants => {
-      // 使用展開運算子（spread operator） ...r，將 r 的物件拷貝並做出做局部修改，在展開之後會出現兩個重複的 description，但當 key 重複時，會被後面出現的取代
-      const data = restaurants.map(r => ({
-        ...r,
-        description: r.description.substring(0, 50) // 將餐廳敘述文字（description）截為 50 個字
-      }))
-      // console.log(data)
-      return res.render('restaurants', {
-        restaurants: data
-      })
     })
+      .then(restaurants => {
+        // 使用展開運算子（spread operator） ...r，將 r 的物件拷貝並做出做局部修改，在展開之後會出現兩個重複的 description，但當 key 重複時，會被後面出現的取代
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50) // 將餐廳敘述文字（description）截為 50 個字
+        }))
+        // console.log(data)
+        return res.render('restaurants', {
+          restaurants: data
+        })
+      })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -12,18 +12,24 @@ const userController = require('../controllers/user-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
+// admin
 // 只要是 admin 相關的路由，都要經過 auth.js 這個 middleware 的權限檢查，通過才能往下走
 router.use('/admin', authenticatedAdmin, admin)
 
+// signup
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 
+// signin
 router.get('/signin', userController.signInPage)
 // 請 Passport 直接做身分驗證
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 
+// logout
 router.get('/logout', userController.logout)
 
+// restaurants
+router.get('/restaurants/:id', authenticated, restaurantController.getRestaurant)
 router.get('/restaurants', authenticated, restaurantController.getRestaurants)
 // 設定 fallback 路由，router.use 在任何HTTP請求方法（GET、POST、PUT等）都能執行
 router.use('/', (req, res) => res.redirect('/restaurants'))
