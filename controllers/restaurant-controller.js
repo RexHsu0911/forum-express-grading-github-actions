d331643d71745d76e65b11a0d41b18e3e5ef22ab
Rex Hsu <aexz5476@gmail.com>
2023-10-06 00:41:25 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,22 @@
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    return Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    }).then(restaurants => {
+      // 使用展開運算子（spread operator） ...r，將 r 的物件拷貝並做出做局部修改，在展開之後會出現兩個重複的 description，但當 key 重複時，會被後面出現的取代
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50) // 將餐廳敘述文字（description）截為 50 個字
+      }))
+      // console.log(data)
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
   }
 }
 
